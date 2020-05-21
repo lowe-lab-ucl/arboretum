d4c55fabae3ed0d653a8389c74ccb75ac7fec6d9
quantumjot <code@arlowe.co.uk>
2020-05-21 07:29:53 +0100

    Updated README and qt_tracks_layer

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ http://lowe.cs.ucl.ac.uk/cellx.html
 
 Dockable widget for [Napari](https://github.com/napari) for visualizing cell track data.
 
+Features:
++ Custom `Track` layer for fast visualization of track data in Napari
++ Lineage tree plot widget
++ Track count widget
++ Integration with bTrack
+
 [![LineageTree](./examples/napari.png)](http://lowe.cs.ucl.ac.uk/cellx.html)  
 *Automated cell tracking and lineage tree reconstruction*.
 
--- arboretum/layers/track/qt_tracks_layer.py
+++ arboretum/layers/track/qt_tracks_layer.py
@@ -13,6 +13,10 @@ from qtpy.QtWidgets import (
 
 import numpy as np
 
+
+MAX_TAIL_LENGTH = 500
+MAX_TAIL_WIDTH = 40
+
 class QtTracksControls(QtLayerControls):
     """Qt view and controls for the arboretum Tracks layer.
 
@@ -34,38 +38,52 @@ class QtTracksControls(QtLayerControls):
         super().__init__(layer)
 
         self.layer.events.edge_width.connect(self._on_edge_width_change)
+        self.layer.events.tail_length.connect(self._on_tail_length_change)
 
-        self.size_slider = QSlider(Qt.Horizontal)
-        self.size_slider.setFocusPolicy(Qt.NoFocus)
-        self.size_slider.setMinimum(0)
-        self.size_slider.setMaximum(100)
-        self.size_slider.setSingleStep(1)
+        # combo box for track coloring
+        self.color_by_combobox = QComboBox()
+        self.color_by_combobox.insertItem(0, 'track ID')
+        self.color_by_combobox.insertItem(1, 'root ID')
+        self.color_by_combobox.insertItem(2, 'parent ID')
+        self.color_by_combobox.insertItem(3, 'generation')
 
+        # slider for track tail length
+        self.tail_length_slider = QSlider(Qt.Horizontal)
+        self.tail_length_slider.setFocusPolicy(Qt.NoFocus)
+        self.tail_length_slider.setMinimum(1)
+        self.tail_length_slider.setMaximum(MAX_TAIL_LENGTH)
+        self.tail_length_slider.setSingleStep(1)
+
+        # slider for track edge width
         self.edge_width_slider = QSlider(Qt.Horizontal)
         self.edge_width_slider.setFocusPolicy(Qt.NoFocus)
         self.edge_width_slider.setMinimum(1)
-        self.edge_width_slider.setMaximum(10)
+        self.edge_width_slider.setMaximum(MAX_TAIL_WIDTH)
         self.edge_width_slider.setSingleStep(1)
 
+        # checkboxes for display
         self.tail_checkbox = QCheckBox()
+        self.tail_checkbox.setChecked(True)
         self.id_checkbox = QCheckBox()
 
 
         self.edge_width_slider.valueChanged.connect(self.change_width)
+        self.tail_length_slider.valueChanged.connect(self.change_tail_length)
         self.tail_checkbox.stateChanged.connect(self.change_display_tail)
         self.id_checkbox.stateChanged.connect(self.change_display_id)
 
         # grid_layout created in QtLayerControls
         # addWidget(widget, row, column, [row_span, column_span])
-
-        self.grid_layout.addWidget(QLabel('opacity:'), 1, 0)
-        self.grid_layout.addWidget(self.opacitySlider, 1, 1)
-        self.grid_layout.addWidget(QLabel('blending:'), 2, 0)
-        self.grid_layout.addWidget(self.blendComboBox, 2, 1)
+        self.grid_layout.addWidget(QLabel('color by:'), 0, 0)
+        self.grid_layout.addWidget(self.color_by_combobox, 0, 1)
+        self.grid_layout.addWidget(QLabel('blending:'), 1, 0)
+        self.grid_layout.addWidget(self.blendComboBox, 1, 1)
+        self.grid_layout.addWidget(QLabel('opacity:'), 2, 0)
+        self.grid_layout.addWidget(self.opacitySlider, 2, 1)
         self.grid_layout.addWidget(QLabel('edge width:'), 3, 0)
         self.grid_layout.addWidget(self.edge_width_slider, 3, 1)
         self.grid_layout.addWidget(QLabel('tail length:'), 4, 0)
-        self.grid_layout.addWidget(self.size_slider, 4, 1)
+        self.grid_layout.addWidget(self.tail_length_slider, 4, 1)
         self.grid_layout.addWidget(QLabel('tail:'), 5, 0)
         self.grid_layout.addWidget(self.tail_checkbox, 5, 1)
         self.grid_layout.addWidget(QLabel('show ID:'), 6, 0)
@@ -74,7 +92,10 @@ class QtTracksControls(QtLayerControls):
         self.grid_layout.setColumnStretch(1, 1)
         self.grid_layout.setSpacing(4)
 
-        
+        self._on_tail_length_change()
+        self._on_edge_width_change()
+
+
 
 
     def _on_edge_width_change(self, event=None):
@@ -87,7 +108,7 @@ class QtTracksControls(QtLayerControls):
         """
         with self.layer.events.edge_width.blocker():
             value = self.layer.edge_width
-            value = np.clip(int(2 * value), 0, 40)
+            value = np.clip(int(2 * value), 1, MAX_TAIL_WIDTH)
             self.edge_width_slider.setValue(value)
 
     def change_width(self, value):
@@ -98,7 +119,31 @@ class QtTracksControls(QtLayerControls):
         value : float
             Line width of shapes.
         """
-        self.layer.edge_width = float(value) / 2
+        self.layer.edge_width = float(value) / 2.
+
+
+    def _on_tail_length_change(self, event=None):
+        """Receive layer model edge line width change event and update slider.
+
+        Parameters
+        ----------
+        event : qtpy.QtCore.QEvent, optional.
+            Event from the Qt context, by default None.
+        """
+        with self.layer.events.tail_length.blocker():
+            value = self.layer.tail_length
+            value = np.clip(value, 1, MAX_TAIL_LENGTH)
+            self.tail_length_slider.setValue(value)
+
+    def change_tail_length(self, value):
+        """Change edge line width of shapes on the layer model.
+
+        Parameters
+        ----------
+        value : float
+            Line width of shapes.
+        """
+        self.layer.tail_length = value
 
 
     def change_display_tail(self, state):
