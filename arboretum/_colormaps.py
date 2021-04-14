024abd4d6919bc70c4d92743d3abea1c6f00931a
quantumjot <code@arlowe.co.uk>
2021-04-14 17:20:14 +0100

    updated colormaps

--- arboretum/_colormaps.py
+++ arboretum/_colormaps.py
@@ -1,14 +1,9 @@
 import numpy as np
-from btrack.constants import Fates, States
-from napari.utils.colormaps import Colormap, AVAILABLE_COLORMAPS
+from napari.utils.colormaps import AVAILABLE_COLORMAPS, Colormap
 
 
 def colormap_bins(cmap: Colormap):
-    return np.linspace(-0.5, cmap.shape[0]-0.5, cmap.shape[0]+1)
-
-
-
-
+    return np.linspace(-0.5, cmap.shape[0] - 0.5, cmap.shape[0] + 1)
 
 
 class ModuloColormap(Colormap):
@@ -27,13 +22,17 @@ class ModuloColormap(Colormap):
 # States.APOPTOSIS = 4          -> red
 # States.NULL = 5               -> orange
 # States.DUMMY = 99             -> orange
-STATE_COLORMAP = np.array([[0.5, 0.5, 0.5, 1.0],
-                           [0.0, 0.0, 1.0, 1.0],
-                           [0.0, 1.0, 0.0, 1.0],
-                           [1.0, 0.0, 1.0, 1.0],
-                           [1.0, 0.0, 0.0, 1.0],
-                           [1.0, 0.65, 0.0, 1.0],
-                           [1.0, 0.65, 0.0, 1.0]])
+STATE_COLORMAP = np.array(
+    [
+        [0.5, 0.5, 0.5, 1.0],
+        [0.0, 0.0, 1.0, 1.0],
+        [0.0, 1.0, 0.0, 1.0],
+        [1.0, 0.0, 1.0, 1.0],
+        [1.0, 0.0, 0.0, 1.0],
+        [1.0, 0.65, 0.0, 1.0],
+        [1.0, 0.65, 0.0, 1.0],
+    ]
+)
 
 # fate colors:
 # Fates.FALSE_POSITIVE = 0      ->
@@ -56,34 +55,42 @@ FATE_COLORMAP = np.array([])
 
 # Survivor -> cyan
 # Deceased -> red
-SURVIVOR_COLORMAP = np.array([[1.0, 0.0, 0.0, 1.0],
-                              [0.0, 1.0, 0.8, 1.0]])
+SURVIVOR_COLORMAP = np.array([[1.0, 0.0, 0.0, 1.0], [0.0, 1.0, 0.8, 1.0]])
 
 
 # steal some colors, mwah ha ha...
-ID_COLORMAP = AVAILABLE_COLORMAPS['turbo'].colors
-ID_COLORMAP = ID_COLORMAP[::4,:]
+ID_COLORMAP = AVAILABLE_COLORMAPS["turbo"].colors
+ID_COLORMAP = ID_COLORMAP[::4, :]
 
 
-id_colormap = ModuloColormap(ID_COLORMAP,
-                             controls=colormap_bins(ID_COLORMAP),
-                             interpolation='zero',
-                             name='tracking_id')
-
-state_colormap = Colormap(STATE_COLORMAP,
-                          controls=colormap_bins(STATE_COLORMAP),
-                          interpolation='zero',
-                          name='tracking_state')
-
-survivor_colormap = Colormap(SURVIVOR_COLORMAP,
-                             controls=colormap_bins(SURVIVOR_COLORMAP),
-                             interpolation='zero',
-                             name='tracking_survivors')
+id_colormap = ModuloColormap(
+    ID_COLORMAP,
+    controls=colormap_bins(ID_COLORMAP),
+    interpolation="zero",
+    name="tracking_id",
+)
 
 
+state_colormap = Colormap(
+    STATE_COLORMAP,
+    controls=colormap_bins(STATE_COLORMAP),
+    interpolation="zero",
+    name="tracking_state",
+)
 
-colormaps = {'ID': id_colormap,
-             'parent': id_colormap,
-             'root': id_colormap,
-             'states': state_colormap,
-             'survivor': survivor_colormap}
+
+survivor_colormap = Colormap(
+    SURVIVOR_COLORMAP,
+    controls=colormap_bins(SURVIVOR_COLORMAP),
+    interpolation="zero",
+    name="tracking_survivors",
+)
+
+
+colormaps = {
+    "ID": id_colormap,
+    "parent": id_colormap,
+    "root": id_colormap,
+    "states": state_colormap,
+    "survivor": survivor_colormap,
+}
