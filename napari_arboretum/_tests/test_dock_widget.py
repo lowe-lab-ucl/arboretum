1ad0dcbedf28f87110663b4e01e3e79208be4ba9
David Stansby <dstansby@gmail.com>
2022-03-15 15:38:48 +0000

    Enable simple widget test

--- napari_arboretum/_tests/test_dock_widget.py
+++ napari_arboretum/_tests/test_dock_widget.py
@@ -8,11 +8,11 @@ MY_PLUGIN_NAME = "napari-arboretum"
 MY_WIDGET_NAMES = ["Arboretum"]
 
 
-# @pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
-# def test_something_with_viewer(widget_name, make_napari_viewer):
-#     viewer = make_napari_viewer()
-#     num_dw = len(viewer.window._dock_widgets)
-#     viewer.window.add_plugin_dock_widget(
-#         plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
-#     )
-#     assert len(viewer.window._dock_widgets) == num_dw + 1
+@pytest.mark.parametrize("widget_name", MY_WIDGET_NAMES)
+def test_adding_widget(widget_name, make_napari_viewer):
+    viewer = make_napari_viewer()
+    num_dw = len(viewer.window._dock_widgets)
+    viewer.window.add_plugin_dock_widget(
+        plugin_name=MY_PLUGIN_NAME, widget_name=widget_name
+    )
+    assert len(viewer.window._dock_widgets) == num_dw + 1
